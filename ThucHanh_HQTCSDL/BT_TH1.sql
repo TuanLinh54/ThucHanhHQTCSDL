6be8b4c623f922f0c448602e57813371b643d891
TuanLinh54 <115730691+TuanLinh54@users.noreply.github.com>
2023-02-15 09:52:35 +0700

    Update BT_TH1.sql

--- ThucHanh_HQTCSDL/BT_TH1.sql
+++ ThucHanh_HQTCSDL/BT_TH1.sql
@@ -74,10 +74,24 @@ ADD CONSTRAINT df_NgayNhap DEFAULT GETDATE() FOR NgayNhap;
 ALTER TABLE SanPham
 ADD CONSTRAINT df_DVT CHECK (DVT = N'KG' or DVT = N'Thùng' or DVT = N'Hộp' or DVT = N'Cái')
  
- --Cau 9:
+--Cau 9:
  INSERT INTO SanPham (Masp, Tensp, NgayNhap, DVT, SoluongTon, DongiaNhap, TyLeHoaHong)
- VALUES ('SP1', N'Bút chì', '2022-08-14', N'Cái', 200, 3000, 15)
+ VALUES ('SP1', N'Bút chì', '2022-08-14', N'Cái', 200, 3000, 0.15)
+ INSERT INTO SanPham (Masp, Tensp, NgayNhap, DVT, SoluongTon, DongiaNhap, TyLeHoaHong)
+ VALUES ('SP2', N'Khẩu Trang', '2022-10-18', N'Thùng', 50, 2500, 0.10)
 
 INSERT INTO KhachHang (MaKH, TenKH, Diachi, Dienthoai)
 VALUES ('KH1', N'Thanh Lam', 'TP.HCM', '0945578442')
+INSERT INTO KhachHang (MaKH, TenKH, Diachi, Dienthoai)
+VALUES ('KH2', N'Toàn Thắng', 'Tây Ninh', '0945566778')
+
+INSERT INTO HoaDon (MaHD, Ngaylap, Ngaygiao, MaKH, Diengiai)
+VALUES ('HD0001', '2022-08-16', '2022-08-25', 'KH1', 'Sản phẩm bút chì loại 1')
+INSERT INTO HoaDon (MaHD, Ngaylap, Ngaygiao, MaKH, Diengiai)
+VALUES ('HD0002', '2022-10-20', '2022-11-05', 'KH2', 'Sản phẩm khẩu trang kháng khuẩn')
+
+INSERT INTO ChiTietHD (MaHD, Masp, Soluong)
+VALUES ('HD0001', 'SP1', 300)
+INSERT INTO ChiTietHD (MaHD, Masp, Soluong)
+VALUES ('HD0002', 'SP2', 800)
 
