44bf7bdf2b2db0ed895e93d22744d7f51b8f0c9c
TuanLinh54 <115730691+TuanLinh54@users.noreply.github.com>
2023-03-15 11:01:56 +0700

    Add files via upload

---
+++ BUOI 5/Lab8_BUOI5.sql
@@ -0,0 +1,35 @@
+﻿----------CAU 1---------
+CREATE PROC spTangLuong
+AS
+    UPDATE NHANVIEN SET Luong = Luong * 1.1
+GO
+
+EXEC spTangLuong
+
+----------CAU 2---------
+ALTER TABLE NHANVIEN
+ADD NgayNghiHuu SMALLDATETIME
+
+CREATE PROC spNghiHuu
+AS
+BEGIN
+    UPDATE NHANVIEN
+    SET NgayNghiHuu = DATEADD(DAY, 100, GETDATE())
+    WHERE (Phai = N'Nam' AND DATEDIFF(YEAR, NgSinh, GETDATE()) >= 60)
+    OR (Phai = N'Nữ' AND DATEDIFF(YEAR, NgSinh, GETDATE()) >= 55)
+END;
+
+EXEC spNghiHuu
+
+----------CAU 3---------
+CREATE PROCEDURE spXemDeAn @DDiemDA NVARCHAR(255)
+AS
+BEGIN
+    SELECT * FROM DeAn
+    WHERE DDiemDA = @DDiemDA
+END;
+
+EXEC spXemDeAn @DDiemDA = N'Dung Quất';
+
+
+----------CAU 4---------
